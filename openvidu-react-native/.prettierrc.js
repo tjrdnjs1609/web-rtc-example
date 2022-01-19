b89a6d9f33eca03bc9fc260786cb2a323ad65802
김석원 <seokworking1609@gmail.com>
2022-01-19 10:24:26 +0900

    install sw

---
+++ openvidu-react-native/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 4,
+  printWidth: 140,
+  bracketSpacing: true,
+  useTabs: true
+};
